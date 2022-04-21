de85c0406e909dfcfb8c6475226e64fc20ba0d54
NyergesC <cintia.nyerges@gmail.com>
2022-04-21 16:24:14 +0200

    fetch

--- src/component/ButtonWithClass.jsx
+++ src/component/ButtonWithClass.jsx
@@ -4,18 +4,72 @@ import React, { Component } from 'react'
 class ButtonWithClass extends Component {
     constructor(){
         // super() köti össze a construktoron belüli dolgokat a class-ok között (mivel extends használunk)
-        super()
+        super() //ha extendelunk egy class egy masikkal akkor ezzel hivjuk meg az o constructorat, tehat mindenkeppen kell
         this.state = {
             key1: 'This is the default state of Class Component',
-            key2: 0
+            key2: 0,
+            title: {
+                main: "main title",
+                sub: 'sub title'
+            },
+            countries: []
         }
+        console.log('1')
     };
+
+    componentDidMount(){
+
+        console.log('3')
+        fetch("https://restcountries.com/v3.1/all")
+            .then(res => res.json())
+                .then(countriesData => this.setState(
+                    () => {
+                        return {countries: countriesData}
+                    },
+                    () => {
+                        console.log(this.state)
+                    }
+                ))
+    }
+    // a setSate egy asszinkron methodus!!!!
     render() {
+        console.log('2')
         return (
             <>
-            <div>{this.state.key1}</div>
+            <div>{this.state.key1}</div> 
             <button onClick={() => this.setState({key1: 'This is the default state of Class Component CLICKED'})}>CLICK</button>
-            <button onClick={() => this.setState({key2:1})}></button>
+           {/*  <button onClick={() =>{
+               this.state.key2=1
+               console.log(this.state) //azert kell {}ba rakni mert uj objektumot hoz letre a lentebb levo sorban aminek a kulcsat adjuk meg
+            }}>{this.state.key2}</button>   */}
+{/*             <button onClick={() => this.setState({key2:1})}></button> */}  
+{/*              <button onClick={() =>{
+             this.setState({
+                 title: {
+                    main:'h1',
+                    sub:'string'                 
+            })
+                console.log(this.state)
+            }}>{this.state.title.main}</button> */}
+             <button onClick={() =>{
+             this.setState(
+                () => {
+                    return {
+                        title: {
+                            main:'h1',
+                            sub:'string'                 
+                }}
+                },         
+                () => {
+                    console.log(this.state)
+                },         
+             )
+             console.log(this.state)
+            }}>{this.state.title.main}</button>
+
+            <div className='countries'>
+                {this.state.countries.map((country, index) => <div key={index}>{country.name.common}</div>)}
+            </div>
             </>
         );
     };
--- src/component/ButtonWithHook.jsx
+++ src/component/ButtonWithHook.jsx
@@ -1,14 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 function ButtonWithHook() {
   const [key1, setKey1] = useState("this is the default state of a function component")
   const [key2, setKey2] = useState(0)
+
+  const [countries, setCountries] = useState([]) //2 tombbel jon vissza, az elso az alap erteke, a masodik amire valtozik, demindketto uj kulon tomb az useState-ben
+
+/*   fetch("https://restcountries.com/v3.1/all")
+  .then(res => res.json())
+  .then(countriesData => setCountries(countriesData)) */
+  
+  /*   console.log('render') */
+  
+  useEffect( //2 argumentum: callback fv, array: amibe ha valtozast irunk akkor ujra fut a useeffect
+  () => {
+        fetch("https://restcountries.com/v3.1/all")
+            .then(res => res.json())
+            .then(countriesData => setCountries(countriesData))
+
+        },
+        []
+    )
+    
+
   return (
       <>
     <div>{key1}</div>
     <button onClick={() => setKey1("This is the NEW state of a function component")}>{key1}</button>
     <button onClick={() => setKey2(1)}>{key2}</button>
+
+    <div className='countries'>
+        {countries.map((country, index) => <div key={index}>{country.name.common}</div>)}
+    </div>
     </>
   )
 }
